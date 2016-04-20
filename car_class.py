5e7b52afb886e7ba33e737910ce9b7ca174d3378
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-04-20 18:23:58 -0400

    Initial commit.  Pseudocode mostly written.

---
+++ car_class.py
@@ -0,0 +1,8 @@
+# car_class.py
+# Exercise Selected: Chapter 14 program 2
+# Name of program: Car Class
+# Description of program:  This program demonstrates OOP programming by
+#   using a class to create and object then drive it around the block.
+
+def main():
+    pass
