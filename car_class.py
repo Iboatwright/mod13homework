bee8e3928c69ca6e58a8c8abdb0eeabe52027438
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-04-21 12:49:47 -0400

    Complete and ready for submit.

--- car_class.py
+++ car_class.py
@@ -4,6 +4,7 @@
 # Description of program:  This program demonstrates OOP programming by
 #   using a class to create and object then drive it around the block.
 
+
 def main():
     # Local variables
     end_program = False
@@ -13,23 +14,32 @@ def main():
 
     while end_program not in ['yes', 'y']:
         # Local variables
+        speed_changes = 5
         year_model = int(get_user_input(
                          "year of the car's model in YYYY format."))
         make = get_user_input("make of the car.")
 
+        # car_car is an object of Car class.
         car_car = Car(year_model, make)
 
+        # Loop through the accelerate function and print the current speed.
         print("Here we go.  Hang on tight!")
-        for i in range(5):
+        for i in range(speed_changes):
             car_car.accelerate()
             print("Current speed is: {}".format(car_car.get_speed()))
 
-        for i in range(5):
+        # Loop through the brake function and print the current speed.
+        for i in range(speed_changes):
             car_car.brake()
             print("Breaking! Speed is: {}".format(car_car.get_speed()))
 
+        # This is a test drive, not a car rental.  Only one ride per turn.
+        #   The while loop ends here and the reference variables pass out of
+        #   scope, effectively destroying them.  If the user goes again new
+        #   variables will be created.
         print("Thanks for test driving the {} {}.".format(year_model, make))
         end_program = go_again()
+    print("Please exit to your left. Have a nice day!")
 
 
 # Displays an introduction to the program and describes what it does.
@@ -39,9 +49,11 @@ def fluffy_intro():
           'object\nand test driving it.')
     return None
 
+
 # Returns unvalidated user input.
 def get_user_input(message):
-    return input("Please enter the {}".format(message))
+    return input("Please enter the {} >> ".format(message))
+
 
 # Program loop control variable with validation.
 def go_again():
@@ -56,34 +68,32 @@ class Car:
     # The init constructor accepts the car’s year model and make as arguments.
     def __init__(self, year_model, make):
         # The year_model field is an Integer that holds the car’s year model.
-        self.__year_model = year_model
+        self.year_model = year_model
 
         # The make field references a String that holds the make of the car.
-        self.__make = make
+        self.make = make
 
         # The speed field is an Integer that holds the car’s current speed.
-        self.__speed = 0
+        self.speed = 0
 
     # accelerate adds 5 or some input integer to the speed field.
     def accelerate(self, a=5):
-        self.__speed += a
+        self.speed += a
 
     # break subtracts 5 or some input integer to the speed field to a minimum
     #   of 0.
     def brake(self, b=5):
-        self.__speed -= b if self.__speed >= b else 0
+        self.speed -= b if self.speed >= b else 0
 
     # Accessors to return class field values.
-    def get_year_model(self):
-        return self.__year_model
+    def year_model(self):
+        return self.year_model
 
     def get_make(self):
-        return self.__make
+        return self.make
 
     def get_speed(self):
-        return self.__speed
-
-
+        return self.speed
 
 
 # Execute the program.
